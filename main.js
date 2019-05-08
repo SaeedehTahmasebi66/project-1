f2271d5666053e90ab9f1f50d94f2830b07d9920
tahmasebi <sara66@protonmail.com>
2019-05-08 17:25:50 +0430

    active the form

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ function changeTheComment(comment) {
      $("#dots i.selected").removeClass("selected");
      $(comment.id).addClass("selected");
 }
+
+//active the form
+ function sendComment() {
+      var userName = $("#user_name").val();
+      //var userName = document.getElementById("user_name").value;
+      var userEmail = $("#user_email").val();
+      alert ("Thank you for yor comment, " + userName + "." +
+      "\nWe will answer you by your email, " + userEmail);
+}
